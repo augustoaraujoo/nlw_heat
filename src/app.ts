b7726f0ccd84d44d6bf62716a796e179873e9a3e
augustoroocha <augustinho_darocha1311@hotmail.com>
2021-10-23 15:11:19 -0200

    backend finalizado

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,41 @@
 import "dotenv/config";
 import express from "express";
-import http from 'http';
-import cors from 'cors'
-import { Server } from 'socket.io';
-import { router } from './routes';
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+
+import { router } from "./routes";
 
 const app = express();
-app.use(cors)
+app.use(cors());
+
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
+
 io.on("connection", (socket) => {
-    console.log(`Usuário conectado no socket ${socket.id}`);
+  console.log(`Usuário conectado no socket ${socket.id}`);
 });
+
 app.use(express.json());
+
 app.use(router);
 
-app.get('/github', (req, res) => {
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
-app.get('/singnin/callback', (req, res) => {
-    const { code } = req.query;
-    return res.json(code);
-})
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
 
-export { serverHttp ,io};
+  return response.json(code);
+});
+
+export { serverHttp, io };
